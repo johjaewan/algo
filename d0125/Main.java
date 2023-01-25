e0b1a9e19bb40b5849eaccd7a9781e4f5a72cad7
user <johjaewan@naver.com>
2023-01-26 00:59:15 +0900

    0125

---
+++ d0125/Main.java
@@ -0,0 +1,30 @@
+package d0125;
+
+public class Main {
+	
+	static int visited[];
+
+	public int solution(int n, int[][] computers) {
+		int answer = 0;
+		
+		visited = new int [n];
+		
+		for (int i = 0; i<n; i++) {
+			if(visited[i] == 0) {
+				answer++;
+				dfs(i, computers,n);
+			}
+		}
+		return answer;
+	}
+	
+	static void dfs (int i, int computers[][], int n) {
+		visited[i] = 1;
+		for(int j = 0; j < n; j++) {
+			if(visited[j] == 0 && computers [i][j] == 1) {
+				dfs(j,computers,n);
+			}
+		}
+	}
+
+}
