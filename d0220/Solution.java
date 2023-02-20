3cbf3d935353da53223771cf67a91de0ab00da3d
user <johjaewan@naver.com>
2023-02-20 23:04:12 +0900

    0220

---
+++ d0220/Solution.java
@@ -0,0 +1,19 @@
+package d0220;
+
+public class Solution {
+	public int solution(int n) {
+        int answer = 0;
+        
+        int dp[] = new int [n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i-1]+ dp[i-2])%1000000007;
+        }
+        
+        
+        answer = dp[n];
+        return answer;
+    }
+}
